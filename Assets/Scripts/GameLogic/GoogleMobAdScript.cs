92dffccfdce8804264649d28ef66da1810489a97
MaxKolac <maksymilian.kolacinski@gmail.com>
2023-06-03 18:44:45 +0200

    Implemented Google MobAd Plugin

---
+++ Assets/Scripts/GameLogic/GoogleMobAdScript.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+internal class GoogleMobAdScript : MonoBehaviour
+{
+    private void Start() => MobileAds.Initialize(initStatus => { });
+}
